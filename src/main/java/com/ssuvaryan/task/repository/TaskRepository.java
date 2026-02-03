98b76a29fc9dada33ce2080173cce4128ca8a384
Serhiy Suvaryan <suvaryanserhiy.ua@gmail.com>
2026-02-04 00:03:17 +0100

    Defined the TaskRepository interface

---
+++ src/main/java/com/ssuvaryan/task/repository/TaskRepository.java
@@ -0,0 +1,11 @@
+package com.ssuvaryan.task.repository;
+
+import com.ssuvaryan.task.domain.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, UUID> {
+}
