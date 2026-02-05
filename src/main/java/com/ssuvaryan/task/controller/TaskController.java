95815342fcfa7e2a0a037d1fc608db95d6c501a6
Serhiy Suvaryan <suvaryanserhiy.ua@gmail.com>
2026-02-05 23:08:14 +0100

    Implemented ListTask endpoint

--- src/main/java/com/ssuvaryan/task/controller/TaskController.java
+++ src/main/java/com/ssuvaryan/task/controller/TaskController.java
@@ -9,10 +9,9 @@ import com.ssuvaryan.task.service.TaskService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/v1/tasks")
@@ -35,5 +34,13 @@ public class TaskController {
         TaskDto createdTaskDto= taskMapper.toDto(task);
 
         return new ResponseEntity<>(createdTaskDto, HttpStatus.CREATED);
+    }
+    @GetMapping
+    public ResponseEntity<List<TaskDto>> listTasks(){
+        List<Task> tasks = taskService.listTasks();
+        List<TaskDto> taskDtos = tasks.stream().map(taskMapper::toDto).toList();
+
+        return  ResponseEntity.ok(taskDtos);
+
     }
 }
--- src/main/java/com/ssuvaryan/task/service/TaskService.java
+++ src/main/java/com/ssuvaryan/task/service/TaskService.java
@@ -3,6 +3,10 @@ package com.ssuvaryan.task.service;
 import com.ssuvaryan.task.domain.CreateTaskRequest;
 import com.ssuvaryan.task.domain.entity.Task;
 
+import java.util.List;
+
 public interface TaskService {
     Task createTask(CreateTaskRequest request);
+
+    List<Task> listTasks();
 }
